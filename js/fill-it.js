88636a6c130aa5779791705a5b7e0fb217e2d9b8
Nahuel Cuesta Luengo <nahuelcuestaluengo@gmail.com>
2013-06-04 08:56:58 -0300

    Added skeleton for fill-it.js file.

---
+++ js/fill-it.js
@@ -0,0 +1,17 @@
+/**
+ * fill-it.js
+ * https://github.com/ncuesta/fill-it.js
+ *
+ * Copyright (c) 2013 Jorge Condomi, Pablo Osso, Nahuel Cuesta Luengo
+ * Licensed under the MIT license.
+ *
+ * https://github.com/ncuesta/fill-it.js/blob/master/LICENSE
+ */
+
+'use strict';
+
+var fillIt = (function() {
+  return function(opts) {
+    // TODO: Fill this :)
+  };
+}());
