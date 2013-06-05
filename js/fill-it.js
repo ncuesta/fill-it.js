5520ed63918ba50d30046a23c9d38807ab816888
Nahuel Cuesta Luengo <nahuelcuestaluengo@gmail.com>
2013-06-05 07:48:51 -0300

    Updated the demo page. Added some initial logic.

--- js/fill-it.js
+++ js/fill-it.js
@@ -10,8 +10,29 @@
 
 'use strict';
 
-var fillIt = (function() {
-  return function(opts) {
-    // TODO: Fill this :)
-  };
-}());
+var fillIt = window.fillIt = (function(d) {
+    return function(opts) {
+        var container = d.createElement('span'),
+            context = getOpt(opts, 'context', d);
+
+        container.className = 'fill-it-container';
+        context.appendChild(container);
+
+        // TODO: Fill this :)
+    };
+
+    function getOpt(opts, key, fallback) {
+        return opts && opts[key] || fallback;
+    }
+}(document));
+
+// jQuery plugin initialization (only if jQuery is present)
+if (typeof jQuery !== 'undefined') {
+    jQuery.fn.fillIt = function(opts) {
+        return this.each(function() {
+            var ctx = document.querySelector($(this).data('target')) || this;
+
+            fillIt($.extend({}, opts, { context: ctx }));
+        });
+    };
+}
--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
+$(document.body).on('click.fill-it', '.fi-btn', function() {
+    var $this = $(this),
+        $target = $($this.data('target')) || $this;
 
+    $target.fillIt();
+
+    return false;
+});
