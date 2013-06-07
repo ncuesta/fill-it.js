31bc8c0f20eac1d5e59db0eaad1872e084a91546
Nahuel Cuesta Luengo <nahuelcuestaluengo@gmail.com>
2013-06-07 19:59:49 -0300

    Added the concept of beverages. Prevented multiple fillings on the same target.

--- js/fill-it.js
+++ js/fill-it.js
@@ -12,13 +12,26 @@
 
 var fillIt = window.fillIt = (function(d) {
     return function(opts) {
-        var container = d.createElement('span'),
-            context = getOpt(opts, 'context', d);
+        var container = d.createElement('div'),
+            context = getOpt(opts, 'context', d),
+            classes = 'fill-it-container full';
 
-        container.className = 'fill-it-container';
-        context.appendChild(container);
+        if (context.className.indexOf(' filled ') !== -1) {
+            // The context has already been filled
+            return;
+        }
+
+        if (getOpt(opts, 'beverage', false)) {
+            classes += ' ' + getOpt(opts, 'beverage');
+        }
 
         // TODO: Fill this :)
+
+        container.className = classes;
+        container.innerHTML = 'beverage';
+        context.appendChild(container);
+
+        context.className += ' filled ';
     };
 
     function getOpt(opts, key, fallback) {
@@ -30,9 +43,11 @@ var fillIt = window.fillIt = (function(d) {
 if (typeof jQuery !== 'undefined') {
     jQuery.fn.fillIt = function(opts) {
         return this.each(function() {
-            var ctx = document.querySelector($(this).data('target')) || this;
+            var $this = $(this),
+                ctx = document.querySelector($this.data('target')) || this,
+                dataOpts = $this.data('options') || {};
 
-            fillIt($.extend({}, opts, { context: ctx }));
+            fillIt($.extend({}, opts, dataOpts, { context: ctx }));
         });
     };
 }
